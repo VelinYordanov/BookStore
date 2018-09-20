e72ca139cc78323918af4b4bcbcffd411e3d8a05
VelinYordanov <velin.iordanov@gmail.com>
2018-09-20 17:58:02 +0300

    Finished work on the web crawler - it can now collect the top authors with their books

--- data/models/book.js
+++ data/models/book.js
@@ -2,7 +2,7 @@ const DataValidator = require('../validator');
 
 module.exports = class Book {
     constructor(title, description, price, author, isbn, cover) {
-        this.title = DataValidator.validateString(title, 1, 50);
+        this.title = DataValidator.validateString(title, 1, 200);
         this.description = DataValidator.validateString(description, 30, 15000);
         this.price = DataValidator.validateNumber(price);
         this.author = DataValidator.validateBookAuthor(author);
--- seed.js
+++ seed.js
@@ -1,14 +1,14 @@
-const ROOT_URL = 'https://www.goodreads.com/';
+const ROOT_URL = 'https://www.goodreads.com';
 
 async function something() {
+    const https = require('https');
     const data = await require('./data');
     const Author = require('./data/models/author');
     const Book = require('./data/models/book');
 
     const getContent = function (url) {
         return new Promise((resolve, reject) => {
-            const lib = url.startsWith('https') ? require('https') : require('http');
-            const request = lib.get(url, (response) => {
+            const request = https.get(url, (response) => {
                 if (response.statusCode < 200 || response.statusCode > 299) {
                     reject(new Error('Failed to load page, status code: ' + response.statusCode));
                 }
@@ -18,33 +18,64 @@ async function something() {
                 response.on('end', () => resolve(body.join('')));
             });
 
-            request.on('error', (err) => reject(err))
+            request.on('error', (err) => reject(err));
         })
     };
 
+
     function getImage(url) {
         return new Promise((resolve, reject) => {
-            const https = require('https');
             var Stream = require('stream').Transform;
             var stream = new Stream();
-            https.get(url, (response) => {
+            const request = https.get(url, (response) => {
                 response.on('data', chunk => stream.push(chunk));
                 response.on('end', () => resolve(stream.read()))
             })
+
+            request.on('error', err => reject(err));
         })
     }
 
-    async function addBook(href) {
+    async function getData(href) {
         const url = ROOT_URL + href;
-        const bookHtml = await getContent(url);
+        try {
+            var bookHtml = await getContent(url);
+        } catch {
+            return;
+        }
+
         const cheerio = require('cheerio');
         const $ = cheerio.load(bookHtml);
         const authorLink = $($('#aboutAuthor .bookAuthorProfile__name a')[0]).attr('href');
-        var author = await getAuthor(authorLink);
+        await getAuthor(authorLink);
+    }
+
+    async function addBook(href, author) {
+        const url = ROOT_URL + href;
+        var bookHtml;
+        try {
+            bookHtml = await getContent(url);
+        } catch {
+            return;
+        }
+
+        const cheerio = require('cheerio');
+        const $ = cheerio.load(bookHtml);
         const bookTitle = $('#bookTitle').text().trim();
-        const bookDescription = $($('#description span')[1]).text().trim();
+        const bookDescription = $($('#description span')[1]).text().trim() || $($('#description span')[0]).text().trim();
         const isbn = $($('span[itemprop=isbn]')[0]).text();
-        const bookCover = await getImage($('#coverImage').attr('src'));
+        if (!(bookDescription && isbn)) {
+            return;
+        }
+
+        const bookCoverUrl = $('#coverImage').attr('src');
+        var bookCover;
+        try {
+            bookCover = await getImage(bookCoverUrl);
+        } catch {
+            return;
+        }
+
         const book = new Book(bookTitle, bookDescription, 30, { id: author._id, name: author.name }, isbn, bookCover);
         author.books.push(book);
         await Promise.all([data.authors.update(author._id, author), data.books.add(book)]);
@@ -52,23 +83,39 @@ async function something() {
 
     async function getAuthor(href) {
         const url = ROOT_URL + href;
-        const authorHtml = await getContent(url);
+        var authorHtml;
+        try {
+            authorHtml = await getContent(url);
+        } catch {
+            return;
+        }
+
         const cheerio = require('cheerio');
         const $ = cheerio.load(authorHtml);
         const authorName = $($('.authorName > span')[0]).text();
-        var authorBio = $($('.aboutAuthorInfo > span')[1]).text();
+        const authorBio = $($('.aboutAuthorInfo > span')[1]).text() || $($('.aboutAuthorInfo > span')[0]).text();
         if (!authorBio) {
-            authorBio = $($('.aboutAuthorInfo > span')[0]).text();
+            return
         }
 
         const authorImageUrl = $($(`img[alt="${authorName}"]`)[0]).attr('src');
-        const authorImage = await getImage(authorImageUrl);
-        let authors = await data.authors.searchAuthors(authorName);
-        if (authors.length) {
-            return authors[0];
+        var authorImage;
+        try {
+            authorImage = await getImage(authorImageUrl);
+        } catch {
+            return;
         }
 
-        return (await data.authors.add(new Author(authorName, authorBio, authorImage))).ops[0];
+        var authors = await data.authors.searchAuthors(authorName);
+        var author;
+        if (authors.length) {
+            author = authors[0];
+        } else {
+            author = (await data.authors.add(new Author(authorName, authorBio, authorImage))).ops[0];
+        }
+
+        var bookHrefs = $('.tableList a.bookTitle').map((_, element) => $(element).attr('href')).get();
+        await Promise.all(bookHrefs.map(x => addBook(x, author)));
     }
 
     const html = await getContent('https://www.goodreads.com/choiceawards/best-fiction-books-2017');
@@ -80,7 +127,7 @@ async function something() {
         bookHrefs.push($(element).attr('href'))
     })
 
-    await Promise.all(bookHrefs.map(addBook));
+    await Promise.all(bookHrefs.map(getData));
 }
 
 something().then(async _ => await process.exit());
